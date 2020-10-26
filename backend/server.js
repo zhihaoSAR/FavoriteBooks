a139e29769b8cda9f883e38c098c618bb8fc2755
zhihaoSAR <49375487+zhihaoSAR@users.noreply.github.com>
2020-10-26 13:50:10 +0100

    añadido servicios a backend
    methods:
    "getBooks"
    "getBook"(id)
    "editBook"(id,title,description)

--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,33 @@ const WebSocket = require('ws');
 var book = require('Book')
 const wss = new WebSocket.Server({ port: 3030 });
 
-function initService()
+var service =
 {
-  wss.on('connection', function connection(ws) {
-    ws.on('message', function incoming(data) {
-      wss.clients.forEach(function each(client) {
-        if (client !== ws && client.readyState === WebSocket.OPEN) {
-          client.send(data);
-        }
-      });
-    });
-  });
+  "getBooks" : function(args,ws)
+              {
+                  ws.send(book.getBooks());
+              },
+  "getBook" : function(args,ws)
+              {
+                ws.send(book.getBook(args.id))
+              },
+  "editBook" : function(args,ws)
+              {
+                book.editBook(args.id,args.title,args.description)
+              }
+
 }
 
+
+function initService()
+{
+  
+  wss.on('connection', function connection(ws) {
+    ws.on('message', function incoming(data) {
+      let args = JSON.parse(data)
+      service[args.method](args,ws)
+    });
+  });
+  setInterval(book.SaveBooks,5000)
+}
+book.LoadBook().then(initService)
